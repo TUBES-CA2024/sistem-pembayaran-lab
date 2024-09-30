9941f88810a98f3d4794379d918caef4fdb34e51
MuhammadAlifMaulanaR <muhalif.maulana17@gmail.com>
2024-09-30 13:32:30 +0800

    Modified in file Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,6 +1,7 @@
 <?php
 
-class Database {
+class Database
+{
     private $host       = DB_HOST;
     private $user       = DB_USER;
     private $pass       = DB_PASS;
@@ -9,54 +10,54 @@ class Database {
     private $dbh;
     private $stmt;
 
-    public function __construct(){
+    public function __construct()
+    {
         // data source name
-        $dsn = 'mysql:host=' .$this->host. ';dbname='.$this->db_name;
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
 
         $option = [
 
             PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 
         ];
 
         try {
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
-        } catch(PDOExeption $e) {
+        } catch (PDOException $e) {
             die($e->getMessage());
         }
     }
 
-    public function query($query){
+    public function query($query)
+    {
         $this->stmt = $this->dbh->prepare($query);
     }
 
-    public function bind($param, $value, $type = null) {
+    public function bind($param, $value, $type = null)
+    {
 
-        if(is_null($type)) {
-            
+        if (is_null($type)) {
+
             switch (true) {
-                case is_int($value) :
+                case is_int($value):
                     $type = PDO::PARAM_INT;
                     break;
 
-                case is_bool($value) :
+                case is_bool($value):
                     $type = PDO::PARAM_BOOL;
                     break;
-            
-                case is_null($value) :
+
+                case is_null($value):
                     $type = PDO::PARAM_NULL;
-                    break;        
+                    break;
 
-                default :
-                    $type = PDO::PARAM_STR;    
-
+                default:
+                    $type = PDO::PARAM_STR;
             }
-
         }
 
         $this->stmt->bindValue($param, $value, $type);
-
     }
 
     public function execute()
@@ -70,7 +71,6 @@ class Database {
 
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-
     }
 
     public function single()
@@ -79,14 +79,8 @@ class Database {
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function rowCount() 
+    public function rowCount()
     {
         return $this->stmt->rowCount();
     }
-
-
-
-
-
-
 }
