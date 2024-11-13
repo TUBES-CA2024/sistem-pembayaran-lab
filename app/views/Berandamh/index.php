83e7cdaccd116584a81fedfe4f778eea64d20f0d
Julisa11 <lisajulisa17@gmail.com>
2024-11-13 19:26:04 +0800

    Modified and add pembayaranmh.php

--- app/views/Berandamh/index.php
+++ app/views/Berandamh/index.php
@@ -1,26 +1,5 @@
 <link rel="stylesheet" href="<?= BASEURL ?>/assets/css/Beranda.css">
 
-<<<<<<< HEAD
-<div class="row p-3">
-    <div class="col-lg-10%">
-        <div class="card p-0 ">
-            <div class="d-flex justify-content-end align-items-center">
-                <div>
-                    <h6 class="text-primary mb-0">FULAN FULAN FULAN</h6>
-                    <small style="padding-left: 48%;">Mahasiswa Mahasiswi</small>
-                </div>
-                <div class="me-3">
-                    <img src="<?= BASEURL ?>/assets/img/profil-icons.png" alt="foto-wajah" width="70px">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-=======
->>>>>>> master
 <div class="d-flex justify-content-between p-3">
 
     <div class="col-lg-3">
