066fa664d078e70e3de2ad177747aa41fe03d9b1
MuhammadAlifMaulanaR <muhalif.maulana17@gmail.com>
2024-11-19 07:58:15 +0800

    modified

--- app/core/App.php
+++ app/core/App.php
@@ -15,7 +15,7 @@ class App
 
         if (empty($url[0])) {
             // controller devault
-            if (file_exists('home.php')) {
+            if (file_exists('login.php')) {
                 $this->controller = $url[0];
                 unset($url[0]);
                 // var_dump($url);
