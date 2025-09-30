6729674de318f51907c44d17ef3abc964aca4061
MuhammadAlifMaulanaR <muhalif.maulana17@gmail.com>
2025-09-30 16:55:25 +0800

    bug from priode3.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
 
-$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url = "https://103.133.36.131";
 $url = explode("/", $url);
 
-$baseurl = 'http://' . $url[2] . '/SIPEMLA';
+$baseurl = 'https://' . $url[2] . '/s/SIPEMLA';
 
 define('BASEURL', $baseurl);
 // DB
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'sipemla');
+define('DB_PASS', 'Sipemla@2025');
 define('DB_NAME', 'sipemla');
 
 $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
